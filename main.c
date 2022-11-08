c076702790f7eedf57efaa3589bd61efc88294d6
kangdabeen <kdb715@naver.com>
2022-11-08 17:08:24 +0900

    06

--- main.c
+++ main.c
@@ -5,26 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	FILE*fp;
-	char str[30];
+	FILE*fp=NULL;
+	char c;
 	
-	fp=fopen("sample.txt","w");
+	fp=fopen("sample.txt","r");
 	
-	printf("input a word:");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str );
-	
-	printf("input a word:");
-	scanf("%s",str);//&필요 없다.  
-	fprintf(fp,"%s\n",str );
-	
-	printf("input a word:");
-	scanf("%s",str);//&필요 없다.  
-	fprintf(fp,"%s\n",str );
-	
-	
+	while ((c=fgetc(fp)) != EOF)
+	putchar(c);
 	
 	fclose(fp);
+	
 	
 	return 0;
 }
